c10c95fb046db50955da3f8191121efb2f046724
tobiasvdorp <113168391+tobiasvdorp@users.noreply.github.com>
2022-10-12 21:24:27 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', event => {
+    const figures = [...document.querySelectorAll(".skill-logos > figure")];
+    const popups = [...document.querySelectorAll(".skill-logos > figure .popups")];
+    figures.forEach(figure => {
+        const popup = figure.querySelector(".popups");
+        const closeButton = figure.querySelector(".close-button");
+        closeButton?.addEventListener('click', event => {
+            popups.forEach(pop => pop.classList.remove("active"));
+        });
+        figure.addEventListener('click', event => {
+            if (event.path[0].classList.contains("popups") || event.path[0].classList.contains("close-button")) return;
+            popups.forEach(pop => pop.classList.remove("active"));
+            popup.classList.toggle("active");
+        });
+    });
+});
+
