fb8456faaf0cf683e52832836edfa55fb0fd9f58
tobiasvdorp <113168391+tobiasvdorp@users.noreply.github.com>
2022-10-19 19:47:27 +0200

    .

---
+++ Three.js
@@ -0,0 +1,98 @@
+
+import * as THREE from "/node_modules/.vite/deps/three.js?v=549d46db";
+import { OrbitControls } from "/node_modules/.vite/deps/three_examples_jsm_controls_OrbitControls.js?v=549d46db";
+
+// Setup
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(35);
+camera.position.setX(-3);
+
+renderer.render(scene, camera);
+
+// Lights
+
+const pointLight = new THREE.PointLight(0x14342b);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0x14342b);
+scene.add(pointLight, ambientLight);
+
+// Helpers
+
+// const lightHelper = new THREE.PointLightHelper(pointLight)
+// const gridHelper = new THREE.GridHelper(200, 50);
+// scene.add(lightHelper, gridHelper)
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+Array(400).fill().forEach(addStar);
+
+// Background
+
+const spaceTexture = new THREE.TextureLoader().load("images/achtergrond3.svg");
+scene.background = spaceTexture;
+
+// // Avatar
+
+// const jeffTexture = new THREE.TextureLoader().load('images/avatar.png');
+
+// const jeff = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: jeffTexture }));
+
+// scene.add(jeff);
+
+// jeff.position.z = -8;
+// jeff.position.x = 0;
+
+// Scroll Animation
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+
+  // jeff.rotation.y += 0.01;
+  // jeff.rotation.z += 0.01;
+
+  camera.position.z = t * -0.02;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+// Animation Loop
+
+function animate() {
+  requestAnimationFrame(animate);
+  // controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
