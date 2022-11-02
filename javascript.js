0392b308c94312625385d8c76068245a0386fde7
tobiasvdorp <113168391+tobiasvdorp@users.noreply.github.com>
2022-11-02 15:42:10 +0100

    .

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
 window.addEventListener('load', event => {
-    const figures = [...document.querySelectorAll(".skill-logos > figure, #project-platen > div")];
-    const popups = [...document.querySelectorAll(".skill-logos > figure .popups, #project-platen figure .popups")];
+    const figures = [...document.querySelectorAll("#project-platen > div")];
+    const popups = [...document.querySelectorAll("#project-platen figure .b")];
     figures.forEach(figure => {
-        const popup = figure.querySelector(".popups");
+        const popup = figure.querySelector(".b");
         const closeButton = figure.querySelector(".close-button");
         closeButton?.addEventListener('click', event => {
             popups.forEach(pop => pop.classList.remove("active"));
         });
         figure.addEventListener('click', event => {
-            if (event.path[0].classList.contains("popups") ||
+            if (event.path[0].classList.contains("b") ||
              event.path[0].classList.contains("close-button")) return;
             popups.forEach(pop => pop.classList.remove("active"));
             popup.classList.toggle("active");
