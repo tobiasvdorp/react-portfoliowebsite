20420c8dd481e740d941ce7c9b2b5704e0b21b1a
Tobias <113168391+tobiasvdorp@users.noreply.github.com>
2023-09-11 21:22:35 +0200

    Lightmode updated

--- react-portfolio/src/components/js/SettingsMenu.js
+++ react-portfolio/src/components/js/SettingsMenu.js
@@ -9,23 +9,28 @@ const SettingsMenu = () => {
   const openMenu = () => {
     setIsOpen(!isOpen);
   };
+
   const closeMenu = () => {
     setIsOpen(false);
   };
 
   return (
-    <div className="settings-menu">
-      <div className="gear">
+    <div
+      className={`settings-menu ${
+        isOpen ? "active animate__animated animate__bounceInLeft" : ""
+      }`}
+    >
+      <div
+        className={`gear ${
+          isOpen ? "active animate__animated animate__backOutLeft" : ""
+        }`}
+      >
         <i
           className="fa-solid fa-gear fa-2xl settings-button"
           onClick={openMenu}
         ></i>
       </div>
-      <div
-        className={`settings-content ${
-          isOpen ? "active animate__animated animate__fadeIn" : ""
-        }`}
-      >
+      <div className={`settings-content ${isOpen ? "active" : ""}`}>
         <div className="left">
           <h4>Settings</h4>
           <button onClick={closeMenu} className="close-button button">
