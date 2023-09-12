4882af2e4411370f8ff332cfc123dc6d17635090
Tobias <113168391+tobiasvdorp@users.noreply.github.com>
2023-09-12 13:50:03 +0200

    Contact styling finished

--- react-portfolio/src/components/js/Contact.js
+++ react-portfolio/src/components/js/Contact.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "../css/contact.css";
+import "../css/responsive.css";
 import WOW from "wowjs";
 import "animate.css";
 
@@ -7,7 +8,6 @@ const ContactForm = () => {
   const wow = new WOW.WOW();
   wow.init();
   const [status, setStatus] = useState("");
-  const [textAreaHeight, setTextAreaHeight] = useState("120px"); // beginhoogte
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -24,22 +24,14 @@ const ContactForm = () => {
       .then((response) => response.json())
       .then((data) => {
         if (data.ok) {
-          setStatus("Bedankt voor je bericht!");
+          setStatus(
+            "Message sent! I will get back to you as soon as possible."
+          );
           form.reset();
         } else {
-          setStatus("Er ging iets mis, probeer het opnieuw.");
+          setStatus("Something went wrong. Please try again.");
         }
       });
-  };
-  useEffect(() => {
-    const textArea = document.getElementById("message");
-    setTextAreaHeight(`${textArea.scrollHeight}px`);
-  }, []);
-
-  const handleTextAreaInput = (e) => {
-    const textArea = e.target;
-    textArea.style.height = "auto";
-    setTextAreaHeight(`${textArea.scrollHeight}px`);
   };
 
   return (
@@ -48,31 +40,39 @@ const ContactForm = () => {
       <h3 className="readmore">"Hello?"</h3>
 
       <form onSubmit={handleSubmit} className="">
-        <div id="name" className="wow animate__animated animate__flipInX">
+        <div className="wow animate__animated animate__flipInX">
           <label htmlFor="name">Name</label>
           <input type="text" id="name" name="name" required />
         </div>
-        <div id="email" className="wow animate__animated animate__flipInX">
+        <div className="wow animate__animated animate__flipInX">
           <label htmlFor="email">Email</label>
           <input type="email" id="email" name="email" required />
         </div>
-        <div id="message" className="wow animate__animated animate__flipInX">
+        <div className="wow animate__animated animate__flipInX">
           <label htmlFor="message">Message</label>
           <textarea
             id="message"
             name="message"
             required
-            style={{ height: textAreaHeight }}
-            onInput={handleTextAreaInput}
+            onInput={function (e) {
+              const target = e.target;
+              target.style.height = "";
+              target.style.height = `${target.scrollHeight}px`;
+            }}
           ></textarea>
         </div>
-        <div id="send" className="wow animate__animated animate__flipInX">
+        <div id="senddiv" className="wow animate__animated animate__flipInX">
           <button type="submit" className="button">
             Send
           </button>
         </div>
-
-        {status && <p>{status}</p>}
+        <div className="statusdiv">
+          {status && (
+            <p className="status animate__animated animate__fadeInLeft">
+              {status}
+            </p>
+          )}
+        </div>
       </form>
     </div>
   );
